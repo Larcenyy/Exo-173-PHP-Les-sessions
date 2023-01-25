90623ec66886055c5b5ea68159c58db53d9f9e97
Rémy <remyc59440@hotmail.com>
2023-01-25 11:28:32 +0100

    kkk

---
+++ save.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['color'])){
+    $color = $_POST['color'];
+    echo "Vous avez choisi du :  " . $color;
+    echo "<body style='background-color: $color;'>";
+    $_SESSION['color'] = $color;
+}
+else{
+    echo 'Aucun couleur choisi';
+}
+?>
+
+
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+session_start();
+$bg_color = $_SESSION['color'];
+echo "<body style='background-color: $bg_color;'>";
